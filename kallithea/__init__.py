96c0bbebe67241dbaaa7c0c937dcf72ceb097d93
Jaroslav Mikulík <byczech@malyjarda.cz>
2025-11-28 12:02:03 +0100

    Version set to 0.7.102

--- kallithea/__init__.py
+++ kallithea/__init__.py
@@ -36,7 +36,7 @@ import celery
 if sys.version_info < (3, 6):
     raise Exception('Kallithea requires python 3.6 or later')
 
-VERSION = (0, 7, 101)
+VERSION = (0, 7, 102)
 BACKENDS = {
     'hg': 'Mercurial repository',
     'git': 'Git repository',
